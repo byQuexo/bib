7c2228926d2c9fce2572f86003f9430a077fdedf
byQuexo <66476537+byQuexo@users.noreply.github.com>
2023-09-30 23:35:01 +0200

    Added Tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 import React , { useEffect, useState } from 'react';
-import { Combobox } from '@headlessui/react';
+import "./globals.css";
+import { Combobox, Tab } from '@headlessui/react';
 import Stundenplan from './Components/Stundenplan';
 import { faGear, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {Datepicker, DatepickerProps} from 'flowbite-react';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../../@/components/ui/tabs"
 
 interface MyComboboxProps {
   people: { id: number; name: string }[];
@@ -63,35 +70,72 @@ export default function Home() {
   };
   return (
     <>
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className=" fixed flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-          <div className="p-6 mt-6 text-left border w-500 rounded-xl shadow-xl">
-          <h1 className="text-6xl font-bold">
-            Stundenplan
-          </h1>
-          <p className="mt-3 text-2xl">
-            Gib dein Klassen oder ein Dozenten Kürzel ein.
-          </p>
-            <div className="flex items-center justify-between gap-4">
-              <a href="/profile" style={{textAlign: 'left'}}><FontAwesomeIcon icon={faGear} size="lg" height={20}/></a>
-              <a href="/notizen" style={{textAlign: 'left'}}><FontAwesomeIcon icon={faPlus} size="lg" height={20}/></a>
-              <Datepicker
-                language="de-DE"
-                datatype='Date'
-                disabled={true}
-                title='COMMING SOON'
-              />
-            </div>
-            <input
-              type="text"
-              onKeyDown={handleKeyDown}
-              className="w-full px-4 py-2 mt-4 text-base text-black transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2">
-            </input>
-            <Stundenplan username={klasse.toLowerCase()}/>
-          </div>
+        <div className="p-6 mt-6 text-left border w-500 rounded-xl shadow-xl dark:bg-gray-900">
+        <Tabs defaultValue='Stundenplan'>
+          <TabsList className="flex justify-center w-full">
+            <TabsTrigger value="Stundenplan" className="w-1/2 font-bold bg-gray-50 px-5 py-3 dark:bg-gray-900 rounded" > 
+              Stundenplan
+            </TabsTrigger>
+            <TabsTrigger value="Wochenübersicht" className="w-1/2 font-bold bg-gray-50 px-5 py-3 dark:bg-gray-900 rounded" >
+              Wochenübersicht
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="Stundenplan">
+                  <h1 className="text-4xl font-bold">
+                    Stundenplan
+                  </h1>
+                  <p className="mt-3 text-xl">
+                    Gib dein Klassen oder ein Dozenten Kürzel ein.
+                  </p>
+                  <br />
+                    <div className="flex items-center justify-between gap-4">
+                      <a href="/profile" style={{textAlign: 'left'}}><FontAwesomeIcon icon={faGear} size="lg" height={20}/></a>
+                      <a href="/notizen" style={{textAlign: 'left'}}><FontAwesomeIcon icon={faPlus} size="lg" height={20}/></a>
+                      <Datepicker
+                        language="de-DE"
+                        datatype='Date'
+                        disabled={true}
+                        title='COMMING SOON'
+                      />
+                    </div>
+                    <input
+                      type="text"
+                      onKeyDown={handleKeyDown}
+                      className="w-full px-4 py-2 mt-4 text-base text-black transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2">
+                    </input>
+                    <Stundenplan username={klasse.toLowerCase()}/>
+          </TabsContent>
+          <TabsContent value="Wochenübersicht">
+                  <h1 className="text-4xl font-bold">
+                    Wochenübersicht 
+                  </h1>
+                  <p className="mt-3 text-xl">
+                    Gib dein Klassen oder ein Dozenten Kürzel ein.
+                  </p>
+                    <div className="flex items-center justify-between gap-4">
+                      <a href="/profile" style={{textAlign: 'left'}}><FontAwesomeIcon icon={faGear} size="lg" height={20}/></a>
+                      <a href="/notizen" style={{textAlign: 'left'}}><FontAwesomeIcon icon={faPlus} size="lg" height={20}/></a>
+                      <Datepicker
+                        language="de-DE"
+                        datatype='Date'
+                        disabled={true}
+                        title='COMMING SOON'
+                      />
+                    </div>
+                    <input
+                      type="text"
+                      onKeyDown={handleKeyDown}
+                      className="w-full px-4 py-2 mt-4 text-base text-black transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2">
+                    </input>
+                    <Stundenplan username={klasse.toLowerCase()}/>
+          </TabsContent>
+        </Tabs>
+        </div>
       </main>
     </div>
-    </>
+  </>
   );
 };
 
